6c838f5c21887435b74a745e6142cabfa7ee65d2
willsaalves <will.saalves@gmail.com>
2022-02-25 10:01:16 -0300

    atualizacao

--- src/Conta.java
+++ src/Conta.java
@@ -1,12 +1,34 @@
 public class Conta {
 
-	double saldo;
-	int agencia;
-	int numero;
-	Cliente titular;
+	private double saldo;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
+	private static int total;
+	
+	public static int getTotal() {
+		return Conta.total;
+	}
+
+	public static void setTotal(int total) {
+		Conta.total = total;
+	}
+
+	public Conta(int agencia, int numero) {
+		
+		Conta.total++;
+		
+		System.out.println("total de conta é "+ Conta.total);
+		this.agencia = agencia;
+		this.numero = numero;
+	}
 
 	public void deposita(double valor) {
 		this.saldo += valor;
+	}
+
+	public double getSaldo() {
+		return this.saldo;
 	}
 
 	public boolean saca(double valor) {
@@ -18,18 +40,55 @@ public class Conta {
 			return false;
 
 	}
-	
-	public boolean transfere(double valor, Conta destino ) {
-		
-		if(this.saldo >= valor) {
+
+	public boolean transfere(double valor, Conta destino) {
+
+		if (this.saldo >= valor) {
 			this.saldo -= valor;
-		
-		destino.deposita(valor);
-		
-		return true;
+
+			destino.deposita(valor);
+
+			return true;
 		}
-	
+
 		return false;
 	}
-}
 
+	public Cliente getTitular() {
+		return titular;
+	}
+
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+
+	public int getAgencia() {
+
+		return this.agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		if (agencia <= 0) {
+
+			System.out.println("não pode numero negativo ou zero");
+			return;
+
+		}
+		this.agencia = agencia;
+	}
+
+	public int getNumero() {
+		return this.numero;
+	}
+
+	public void setNumero(int numero) {
+		if (agencia <= 0) {
+
+			System.out.println("não pode numero negativo ou zero");
+			return;
+		}
+
+		this.numero = numero;
+	}
+
+}
--- src/TestaMetodo.java
+++ src/TestaMetodo.java
@@ -3,27 +3,35 @@ public class TestaMetodo {
 
 	public static void main(String[] args) {
 
+		Conta contadoWillian = new Conta( 1337, 24255);
+		Conta contadoWillian1 = new Conta( 1337, 123);
+		Conta contadoWillian2 = new Conta( 1337, 1423);
+
 		Cliente Cli = new Cliente();
 
-		Cli.nome = "Willian de Sá";
-		Cli.cpf = "47667890838";
-		Cli.endereco = "rua dos alfaiates 2011";
-		Cli.profissao = "programador";
+		Cli.setNome("Willian de Sá");
+		Cli.setCpf("47667890838");
+		Cli.setEndereco("rua dos alfaiates 2011");
+		Cli.setProfissao("programador");
 
-		Conta contadoWillian = new Conta();
-
-		contadoWillian.deposita(1000);
 		
-		contadoWillian.titular = Cli;
-		System.out.println(contadoWillian.titular.nome);
+		contadoWillian.deposita(1000);
+
+		contadoWillian.setTitular(Cli);
+		System.out.println(contadoWillian.getTitular().getNome());
 
 		boolean retirar = contadoWillian.saca(250);
 
 		if (retirar) {
-			System.out.println(contadoWillian.saldo);
+			System.out.println(contadoWillian.getSaldo());
 		} else {
 			System.out.println("ta pobre po");
 		}
-
+		
+		System.out.println(Conta.getTotal());
+		
+		contadoWillian.setAgencia(30);
+		contadoWillian.setNumero(30);
+		
 	}
 }
